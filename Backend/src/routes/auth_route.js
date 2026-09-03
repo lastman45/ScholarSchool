072562e4e481cf19b36e3a04ee4569dd6ef85a5d
Oliver Okeyo <oliverokeyo21@gmail.com>
2026-09-03 10:25:37 +0300

    initial commit

---
+++ Backend/src/routes/auth_route.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const router = express.Router();
+
+//Endpoints
+router.get("/signup", (req,res) => {
+    res.send("Sign Up Endpoint");
+})
+
+router.get("/login", (req,res) => {
+    res.send("Login Endpoint");
+})
+
+router.get("/logout", (req,res) => {
+    res.send("Logout Endpoint");
+})
+
+export default router;
