16292489b4f35aa0c64c48c0cbf7823bac631bb4
Oliver Okeyo <oliverokeyo21@gmail.com>
2026-09-08 10:08:20 +0300

    Welcome email implemented

--- Backend/src/lib/utils.js
+++ Backend/src/lib/utils.js
@@ -1,8 +1,9 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
+import { ENV } from "./env.js";
 
 export const generateToken = (userId,res) => {
     //user token: if authenticated or not
-    const { JWT_SECRET } = process.env;
+    const { JWT_SECRET } = ENV;
     if(!JWT_SECRET) {
         throw new Error("JWT_SECRET is not configured");
     }
@@ -15,7 +16,7 @@ export const generateToken = (userId,res) => {
         maxAge: 7 * 24 * 60 * 60 * 1000, //MS: 7days...
         httpOnly: true, //prevents XSS attacks
         sameSite: "strict", //CSRF attacks
-        secure: process.env.Node_ENV === "development" ? false : true,
+        secure: ENV.Node_ENV === "development" ? false : true,
     });
 
     return token;
