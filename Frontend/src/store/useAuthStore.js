6aa3bcf1cb23c1ffef3de166e4bdbf14434bbbdf
Oliver Okeyo <oliverokeyo21@gmail.com>
2026-09-14 09:49:52 +0300

    frontend codebase setup

---
+++ Frontend/src/store/useAuthStore.js
@@ -0,0 +1,12 @@
+//Box to store all of our global states
+import { create } from "zustand";
+
+export const useAuthStore = create((set) => ({
+    authUser: { name: "john", _id: 123, age: 25},
+    isLoggedIn: false,
+
+    login: () => {
+        console.log("We just logged in");
+        set({ isLoggedIn: true});
+    },
+}));
